f3553cef0f62fec374c8f5b311e7dde4ec9b7691
821-1 <89720445+821-1@users.noreply.github.com>
2021-08-31 12:25:57 +0900

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,6 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    # 基本的にここにコードを書いていきます。
     if message = "ありがとう":
     line_bot_api.reply_message(
         event.reply_token,
